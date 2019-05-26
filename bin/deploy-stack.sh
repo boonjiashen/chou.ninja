c235dd6c3d04b7c783dd6f735ca3895f8ae12712
Jia-Shen Boon <jiashen@gmail.com>
2019-05-26 15:07:16 +0900

    Flesh out APIG CFN

--- bin/deploy-stack.sh
+++ bin/deploy-stack.sh
@@ -2,8 +2,11 @@
 
 export AWS_DEFAULT_REGION="ap-northeast-1"
 
-STACK_NAME="api-gateway-5"
+STACK_NAME="api-gateway-6"
 TEMPLATE_FILE="cloudformation/stack.yml"
+
+aws cloudformation validate-template  \
+    --template-body "file://${TEMPLATE_FILE}"
 
 aws cloudformation deploy  \
     --template-file ${TEMPLATE_FILE}  \
